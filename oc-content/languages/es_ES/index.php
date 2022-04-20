8c51da018ee91ed740ad583523a6c879a1b8ba0e
alconmar <alvarocontrerasmartinez@gmail.com>
2022-04-20 23:18:20 +0200

    Web tablón anuncios

---
+++ oc-content/languages/es_ES/index.php
@@ -0,0 +1,15 @@
+<?php
+function locale_es_ES_info() {
+  return array(
+     'name'            => 'Spanish (Spain)'
+    ,'short_name'      => 'Spanish'
+    ,'description'     => 'Spanish (Spain) translation'
+    ,'version'         => '3.5.0'
+    ,'author_name'     => 'OsclassPoint'
+    ,'author_url'      => 'https://osclass.osclasspoint.com/'
+    ,'currency_format' => '{NUMBER} {CURRENCY}'
+    ,'date_format'     => 'm/d/Y'
+    ,'stop_words'      => ''
+  );
+}
+?>
